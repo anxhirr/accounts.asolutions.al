7b5696bff00b5f952af0012aa9b4efbe01b2a2e9
anxhi <anxhirr@gmail.com>
2024-08-16 19:31:45 +0200

    chore: set sb cookie

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm, LoginSchemaType } from "@/components/form";
 import { AuthWrapper } from "@/components/wrappers";
 import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 type Args = {
   searchParams: { redirectUrl?: string };
@@ -9,8 +10,21 @@ async function LoginPage({ searchParams }: Args) {
   const signIn = async (values: LoginSchemaType) => {
     "use server";
     const supabase = createClient();
-    const { error } = await supabase.auth.signInWithPassword(values);
+    const { data, error } = await supabase.auth.signInWithPassword(values);
     if (error) throw error;
+    if (process.env.NODE_ENV === "production") {
+      console.log("Setting cookie");
+      cookies().set("sb-auth", JSON.stringify(data.session), {
+        // cookie options
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+        path: "/",
+        domain: ".asolutions.al",
+        expires: new Date(data.session?.expires_at?.toString() || 0),
+      });
+      console.log("Cookie set");
+    }
     const path = searchParams.redirectUrl || `/`;
     return { path };
   };
