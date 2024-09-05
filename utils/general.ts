5c1d8a6165810a72d38c14aed3a5b2ac181461e8
anxhi <anxhirr@gmail.com>
2024-09-05 16:12:28 +0200

    fix: never expire cookie

--- utils/general.ts
+++ utils/general.ts
@@ -2,8 +2,10 @@ import { Session } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
 export const setSbCookie = (session: Session) => {
+  console.log("session", session);
   if (process.env.NODE_ENV === "production") {
     console.log("Setting sb-auth cookie");
+    const maxAge = 100 * 365 * 24 * 60 * 60; // 100 years, never expires
     cookies().set("sb-auth", JSON.stringify(session), {
       // cookie options
       httpOnly: true,
@@ -11,6 +13,7 @@ export const setSbCookie = (session: Session) => {
       sameSite: "lax",
       path: "/",
       domain: ".asolutions.al",
+      maxAge,
       // expires: new Date(session?.expires_at?.toString() || 0),
     });
     console.log("Cookie set");
